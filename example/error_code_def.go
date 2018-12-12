3a6a8ede206389a3c0a3e797dc36d6588e991132
zrw <zrw2010@gmail.com>
2018-12-12 16:03:58 +0800

    feat: add example

--- example/error_code_def.go
+++ example/error_code_def.go
@@ -1,3 +1,3 @@
 package example
 
-//go:generate errcodegen --err_func=Errorf --err_func_pkg=fmt --pkg=example
+//go:generate errcodegen --err_func=Errorf --err_func_pkg=fmt --pkg=example --err_def=error_code_def.conf
