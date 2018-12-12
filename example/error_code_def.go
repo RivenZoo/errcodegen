346cb71b0a320a9b44cab1abde070661a0e2cb9c
zrw <zrw2010@gmail.com>
2018-12-12 16:22:56 +0800

    docs: add comment

--- example/error_code_def.go
+++ example/error_code_def.go
@@ -1,3 +1,4 @@
+// This file is used to trigger go generate. DO NOT ADD OTHER CODE HERE.
 package example
 
 //go:generate errcodegen --err_func=Errorf --err_func_pkg=fmt --pkg=example --err_def=error_code_def.conf
